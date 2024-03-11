eccc47f0053078d7c1f61f721899952ac8e85047
TheDimasikPro <dima.site1806@gmail.com>
2024-03-11 10:25:16 +0500

    add relationship

--- app/Http/Controllers/LocationTravelController.php
+++ app/Http/Controllers/LocationTravelController.php
@@ -8,6 +8,6 @@ class LocationTravelController extends Controller
 {
 	public function index()
 	{
-		return LocationTravel::with('addedStops', 'prices')->get();
+		return LocationTravel::with('addedStops', 'prices.carType')->get();
 	}
 }
